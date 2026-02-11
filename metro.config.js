856e6e09449bab807ea7d941fecae21cd3ee7e8e
mateo.montesinos.j <mateo.montesinos.j@gmail.com>
2026-02-11 00:51:02 +0000

    fix: eliminar app-example y agregar alias de módulos en metro.config.js
    - Eliminar app-example/ que Expo Router escaneaba como rutas adicionales causando la pantalla Hello World
    - Agregar resolver.alias en metro.config.js para @src y @ apuntando a src/ y raíz
    - Completar resolución de módulos tanto en Babel como en Metro

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,13 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require('nativewind/metro');
- 
-const config = getDefaultConfig(__dirname)
- 
-module.exports = withNativeWind(config, { input: './global.css' })
+const path = require('path');
+
+const config = getDefaultConfig(__dirname);
+
+// Resolver aliases para @src/*
+config.resolver.alias = {
+  '@src': path.resolve(__dirname, 'src'),
+  '@': path.resolve(__dirname),
+};
+
+module.exports = withNativeWind(config, { input: './global.css' });
